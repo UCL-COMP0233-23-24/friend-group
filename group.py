b57e48efc2cc6211f5ab325b2140866dd54ca83e
dan <xwj.harry@gmail.com>
2023-10-19 11:23:05 +0100

    to be checked

--- group.py
+++ group.py
@@ -7,25 +7,25 @@ my_group = [
         "name": "Jill",
         "age": "26",
         "job": "biologist",
-        "relationship": {"friend": ["John","Zalika"]},
+        "relationship": {"partner": ["John"],"friend": ["Zalika"]},
     },
     {
         "name": "Zalika",
         "age": "28",
         "job": "cleaner",
-        "relationship": {"friend": ["Jill"]},
+        "relationship": {"friend": ["Jill"],"landlord": ["Nash"]},
     },
     {
         "name": "John",
         "age": "27",
         "job": "cleaner",
-        "relationship": {"friend": ["Jill"]},
+        "relationship": {"partner": ["Jill"],"cousin":["Nash"]},
     },
     {
         "name": "Nash",
         "age": "34",
         "job": "cleaner",
-        "relationship": {"friend": ["John","Zalika"]},
+        "relationship": {"friend": ["Zalika"],"cousin":["John"]},
     },
 ]
 
