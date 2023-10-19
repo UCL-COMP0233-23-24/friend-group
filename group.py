9085895fe4e440b2a8729927d273e7c3c495e37d
Hanze Zhang <hanzezhang0907@gmail.com>
2023-10-19 10:59:32 +0100

    update

--- group.py
+++ group.py
@@ -2,10 +2,10 @@
 
 # Your code to go here...
 
-my_group = [{"name": "HanzeZhang", "age": "22", "job": "cook", "friend": ["WeijieXie"]}, {
-    "name": "WeijieXie", "age": "22", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
-    {"name": "Jill", "age": "26", "job": "biologist", "friend": ["HanzeZhang","Zalika"]},
-    {"name": "Zalika", "age": "28", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
-    {"name": "John", "age": "27", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
-    {"name": "Nash", "age": "34", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]}
+my_group = [{"name": "HanzeZhang", "age": "22", "job": "security", "friend": ["WeijieXie"]},
+            {"name": "WeijieXie", "age": "22", "job": "cleaner", "friend": ["HanzeZhang"]},
+            {"name": "Jill", "age": "26", "job": "biologist", "friend": ["John","Zalika"]},
+            {"name": "Zalika", "age": "28", "job": "artist", "friend": ["Jill"]},
+            {"name": "John", "age": "27", "job": "writer", "friend": ["Jill"]},
+            {"name": "Nash", "age": "34", "job": "chef", "friend": ["John","Zalika"]}
     ]
