048dfc0fb48cb7fb6c088fc1d3384bace1bc6964
TheIronA <alwi.al-haddad.20@ucl.ac.uk>
2023-10-19 10:43:14 +0100

    added relattions

--- group.py
+++ group.py
@@ -7,14 +7,23 @@ class person :
         self.name = name
         self.age = age
         self.job = job
+        self.relations = []
+    
+    def add_relation(self, name, relation):
+        self.relations.append({name:relation})
+
 
 Jill = person('Jill', 26, 'Biologist')
+Jill.add_relation('Zalika','friend')
+Jill.add_relation('John','partner')
+
 Zalika = person('Zalika', 28, 'Artist')
+Zalika.add_relation('Jill','friend')
+
 John = person('John', 27, 'Writer')
+John.add_relation('Jill','partner')
+
 Nash = person('Nash', 34, 'Chef')
+Nash.add_relation('John','cousin')
+Nash.add_relation('Zalika','landlord')
 
-
-
-print(Jill.name)
-
-#my_group = 
