9aa2dc2bb7b823c655700403c1cfb0a1d53eda9c
Radostin <radostinp545@gmail.com>
2023-10-19 10:28:59 +0100

    Created the data structure

--- group.py
+++ group.py
@@ -2,4 +2,56 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+
+    'Jill' : {
+        'age': '26',
+        'job': 'biologist',
+        'connections' : {
+            'friends' : 'Zalika',
+            'partner' : 'John',
+            'landlord' : '',
+
+        }
+         
+    },
+
+    'Zalika' : {
+        'age': '28',
+        'job': 'artist',
+        'connections' : {
+            'friends' : 'Jill',
+            'partner' : '',
+            'landlord' : 'Landlord',
+
+        }
+         
+    },
+
+    'John' : {
+        'age': '27',
+        'job': 'writer',
+        'connections' : {
+            'friends' : '',
+            'partner' : 'Jill',
+            'landlord' : '',
+
+        }
+         
+    },
+
+    'Nash' : {
+        'age': '34',
+        'job': 'chef',
+        'connections' : {
+            'relationship' : {
+                'cousin' : 'John'
+            },
+            'friends' : '',
+            'partner' : '',
+            'landlord' : ''
+
+        }
+         
+    }
+}
