5f37477e23e05dbca0dbf31a3a7091cfd1d3486b
Tianyang Xie <994220475@qq.com>
2023-10-19 10:40:38 +0100

    Change the relation with the name

--- group.py
+++ group.py
@@ -2,4 +2,13 @@
 
 # Your code to go here...
 
-my_group =
+
+Jill={"name":"Jill","age":26,"Job":"Biologist","Relation":[{"Zalika":"friend"},{"John":"partner"}]}
+Zalika={"name":"Zalika","age":28,"Job":"artist","Relation":[{"Jill":"friend"},{"Nash":"renter"}]}
+John={"name":"John","age":27,"Job":"writer","Relation":[{"Jill":"partner"},{"Nash":"uncle"}]}
+Nash={"name":"Nash","age":34,"Job":"chef","Relation":[{"John":"cousin"},{"Zalika":"landlord"}]}
+
+
+my_group =[]
+my_group.append([Jill,Zalika,John,Nash])
+print(my_group)
