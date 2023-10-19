3c436ef9e2bc75d14d167680bd2315e45eeb80af
“Weibin <japi9008@gmail.com>
2023-10-19 11:06:56 +0100

    first edit

--- group.py
+++ group.py
@@ -2,4 +2,33 @@
 
 # Your code to go here...
 
-my_group =
+
+my_group = {'Jill':{
+    'occupation': 'biologist',
+    'friend': 'Zalika',
+    'partner':'John',
+    'age':'26'
+},
+'Zalika':{
+    'age': '28',
+    'occupation': 'artist',
+    'friend':'Jill',
+    'landlord':'Nash'
+},
+'John':{
+'age':'27',
+'occupation': 'writer',
+'partner':'Jill',
+'relative': 'Nash'
+
+},
+'Nash':{
+    'age':'34',
+    'occupation':'chef',
+    'relative':'John',
+    'tenant': 'Zalika'
+}
+
+
+
+}
