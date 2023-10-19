661ee06deb6e0e4e80f8d64378c3184b88229f2b
TheIronA <alwi.al-haddad.20@ucl.ac.uk>
2023-10-19 09:57:39 +0100

    autofilled namess

--- group.py
+++ group.py
@@ -2,4 +2,8 @@
 
 # Your code to go here...
 
-my_group = [{'name': 'John'}]
+my_group = [{'name': 'Jill', 'age': 26, 'job': 'biologist', 'friends': 'Zalika', 'partner': 'John'}, 
+            {'name': 'Zalika', 'age': 28, 'job': 'artist', 'friends': 'Jill'},
+            {'name': 'John', 'age': 27, 'job': 'writer', 'partner': 'Jill'},
+            {'name': 'Nash', 'age': 34, 'job': 'chef', 'landlord': 'Zalika', 'cousin': 'John'},
+            ]
