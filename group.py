3e427ee80ff91c61cc29fb76f9f56f23b1a6e806
Kathy <kathy.xintang@gmail.com>
2023-10-19 10:55:45 +0100

    nearly finish part1

--- group.py
+++ group.py
@@ -2,4 +2,32 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+ 'Jill': {
+  'age' : '26', 'job' : 'bioligist',
+      'relationship':{
+      'Jill_friend' : 'Zalika', 'Jill_partner' : 'John'}
+ }
+ 'Zalika': {
+     'age' : '28', 'job' : 'artist', 
+      'relationship':{
+          'Zalika_friend': 'Jill'}
+ }
+ 'John': {
+     'age' : '27', 'job': 'writer', 
+     'relationship':{
+         'John_friend' : 'Jill'}
+     }
+ 'Nash' : {
+     'age' : '34', 'job' : 'chef',
+     'relationship' :{
+         'Nash_cosin' : 'John', 'Nash'
+     }
+ }
+}
+        
+
+
+
+
+
