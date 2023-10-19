7489530b9782ffe85d1c1613c4b19820eef6720b
dan <xwj.harry@gmail.com>
2023-10-19 10:52:32 +0100

    draft for collaboration

--- group.py
+++ group.py
@@ -2,5 +2,10 @@
 
 # Your code to go here...
 
-my_group = [{"name": "Hanze Zhang", "age": "22", "job": "cook", "friend": "Weijie Xie"}, {
-    "name": "Weijie Xie", "age": "22", "job": "cleaner", "friend": "Hanze Zhang"}]
+my_group = [{"name": "HanzeZhang", "age": "22", "job": "cook", "friend": ["WeijieXie"]}, {
+    "name": "WeijieXie", "age": "22", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
+    {"name": "Jill", "age": "26", "job": "biologist", "friend": ["HanzeZhang","Zalika"]},
+    {"name": "Zalika", "age": "28", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
+    {"name": "John", "age": "27", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]},
+    {"name": "Nash", "age": "34", "job": "cleaner", "friend": ["HanzeZhang","Zalika"]}
+    ]
