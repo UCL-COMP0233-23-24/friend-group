d8270b54d121ad4882e7311ac129af45ee218151
Sophie Morrison <sophie.morrison.23@ucl.ac.uk>
2023-10-19 10:53:54 +0100

    revised structure

--- group.py
+++ group.py
@@ -2,38 +2,48 @@
 
 # Your code to go here...
 
+my_group = {
+    'Jill' : {
+        'age' : 26,
+        'job' : 'biologist',
+        'relationships' : {
+            'friend' : 'Zalika',
+            'partner' : 'John',
+            'cousin' : '',
+            'landlord' : ''
+        }
+    },
+    'Zalika' : {
+        'age' : 28,
+        'job' : 'artist',
+        'relationships' : {
+            'friend' : 'Jill',
+            'partner' : '',
+            'cousin' : '',
+            'landlord' : 'Nash'   
+        }
+    },
+    'John' : {
+        'age' : 27,
+        'job' : 'writer',
+        'relationships' : {
+            'friend' : '',
+            'partner' : 'Jill',
+            'cousin' : 'Nash',
+            'landlord' : ''   
+        }
+    },
+    'Nash' : {
+        'age' : 34,
+        'job' : 'chef',
+        'relationships' : {
+            'friend' : '',
+            'partner' : '',
+            'cousin' : 'John',
+            'landlord' : ''   
+        }
+    }
+}
 
-Jill = {"Name": "Jill",
-        "Age": 26, 
-        "Jobs": "Biologist",
-        "Friend": "Zalika",
-        "Partner": "John",
-        "Cousin": "",
-        "Landlord": ""}
-
-Zalika = {"Name": "Zalika",
-        "Age": 28,  
-        "Jobs": "Artist",
-        "Friend": "Jill",
-        "Partner": "",
-        "Cousin": "",
-        "Landlord": "Nash"}
-
-John = {"Name": "John",
-        "Age": 27,  
-        "Jobs": "Writer",
-        "Friend": "",
-        "Partner": "Jill",
-        "Cousin": "Nash",
-        "Landlord": ""}
-
-Nash = {"Name": "Nash",
-        "Age": 34,  
-        "Jobs": "Chef",
-        "Friend": "",
-        "Partner": "",
-        "Cousin": "John",
-        "Landlord": ""}
-
-my_group = [Jill, Zalika, John, Nash]
+{name: person['relationships'] for name, person in my_group.items()}
 print(my_group)
