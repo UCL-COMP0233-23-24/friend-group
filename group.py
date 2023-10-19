806a52752697d8debedfd2ab9b84a5cea9e9935e
HuangmouIII <zcahyh7@ucl.ac.uk>
2023-10-19 10:25:34 +0100

    finish

--- group.py
+++ group.py
@@ -2,4 +2,18 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {"Jill", "Zalika","John","Nash","Huangmou"}
+Jill = {"age":26,"job": "biologist","friend":"Zalika","partner":"John"}
+Zalika = {"age":28,"job": "artist","friend":"Jill"}
+John = {"age":27,"job": "writer","partner":"Jill"}
+Nash = {"age":34,"job": "chef","cousin":"John","Landlord":"Zalika"}
+Huangmou = {"age":21}
+
+name_of_people = Jill
+index = "Landlord"
+
+if index in name_of_people:
+    print(name_of_people[index])
+else:
+    print(None)
+
