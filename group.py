ce82a9520448bc4abe3eef01522704ef759e2c74
benzi604 <benzi604@outlook.com>
2023-10-19 18:12:25 +0100

    Add three functions

--- group.py
+++ group.py
@@ -36,3 +36,42 @@ my_group = {
             "job" : "chef"}    
 
     }
+
+# Remove the connection between two people in the group
+def forget(person1, person2):
+    for name, info in my_group.items():
+        if name == person1:
+
+            for key, value in info['connection'].items():
+                if value == person2:
+                    info['connection'].pop(key)
+                    break
+
+    for name, info in my_group.items():    
+        if name == person2:
+
+            for key, value in info['connection'].items():
+                if value == person1:
+                    info['connection'].pop(key)
+                    break
+
+
+
+# Add a new person with the given characteristics to the group
+def add_person(name, age, job, relations:dict):
+    my_group[name] = {
+        "connection" : relations,
+        "age" : age,
+        "job" : job
+    }
+
+
+
+# Calculate the mean age for the group
+def average_age():
+    ages = {info['age'] for name, info in my_group.items()}
+    sum = 0
+    for age in ages:
+        sum += age
+        average = sum / len(ages)
+    return average
