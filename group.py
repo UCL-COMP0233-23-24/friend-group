dce7e16ac21838db942c38e07ff6e15079151c17
Michal <miguelpiotrak@gmail.com>
2023-10-19 10:01:56 +0100

    Edit group structure

--- group.py
+++ group.py
@@ -2,4 +2,12 @@
 
 # Your code to go here...
 
-my_group =
+my_group ={
+            "Jill": [26,"biologist",{"friend": "Zalika","partner": "John"}],
+            "Zalika": [28, "artist", {"friend": "Jill"} ],
+            "John": [27,"writer", {"partner": "Jill", "cousin": "Nash"}],
+            "Nash": [34,"chef", {"cousin": "John", "landlord": "Zalika"}]
+
+        }
+print(my_group)
+    
