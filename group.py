3348defdacdf30819bc7802b3db6fe6af9f291db
benzi604 <benzi604@outlook.com>
2023-10-19 10:26:06 +0100

    Created a dictionary of names

--- group.py
+++ group.py
@@ -2,4 +2,45 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+
+        "Jill" : 
+            {"connection" :{
+                "friend" : "Zalika", 
+                "partner" : "John"
+            },
+            "age" : 26,
+            "job" : "biologist"},
+
+            
+        "Zalika" : 
+            {"connection" :{
+                "friend" : "Jill"
+            },
+            "age" : 28,
+            "job" : "artist"},
+
+
+       "John" : 
+            {"connection" :{ 
+                "partner" : "Jill"
+            },
+            "age" : 27,
+            "job" : "writer"},
+
+
+        "Nash" : 
+            {"connection" :{
+                "cousin" : "John", 
+                "landlord" : "Zalika"
+            },
+            "age" : 34,
+            "job" : "chef"}    
+
+    }
+
+
+
+a = {name: info['age'] for name, info in my_group.items()}
+
+print(a)
