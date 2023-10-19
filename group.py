816ce8d5fcb6ceaff99f327bcf06f03ca7354a26
Sophie Morrison <sophie.morrison.23@ucl.ac.uk>
2023-10-19 10:31:24 +0100

    adding data

--- group.py
+++ group.py
@@ -2,4 +2,38 @@
 
 # Your code to go here...
 
-my_group =
+
+Jill = {"Name": "Jill",
+        "Age": 26, 
+        "Jobs": "Biologist",
+        "Friend": "Zalika",
+        "Partner": "John",
+        "Cousin": "",
+        "Landlord": ""}
+
+Zalika = {"Name": "Zalika",
+        "Age": 28,  
+        "Jobs": "Artist",
+        "Friend": "Jill",
+        "Partner": "",
+        "Cousin": "",
+        "Landlord": "Nash"}
+
+John = {"Name": "John",
+        "Age": 27,  
+        "Jobs": "Writer",
+        "Friend": "",
+        "Partner": "Jill",
+        "Cousin": "Nash",
+        "Landlord": ""}
+
+Nash = {"Name": "Nash",
+        "Age": 34,  
+        "Jobs": "Chef",
+        "Friend": "",
+        "Partner": "",
+        "Cousin": "John",
+        "Landlord": ""}
+
+my_group = [Jill, Zalika, John, Nash]
+print(my_group)
