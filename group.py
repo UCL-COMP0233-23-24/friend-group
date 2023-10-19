8786d6c779a0a8f6d81237aed00a75f44657dc8d
Tianyang Xie <994220475@qq.com>
2023-10-19 10:26:41 +0100

    Group creation！

--- group.py
+++ group.py
@@ -2,4 +2,30 @@
 
 # Your code to go here...
 
-my_group =
+
+class person:
+    def __init__(self,name,age,job="",relationship={}):
+        self.name=name
+        self.age=age
+        self.job=job
+        self.relationship=relationship
+    
+    def create_relationship(self,b,relation):
+        b.relationship[self.name]=relation
+        self.relationship[b.name]=relation
+        
+
+    def showall(self):
+        print(self.name,self.age,self.job,self.relationship)
+
+
+Jill = person("Jill",26, "biologist")
+Zalika = person("Zalika",28,"artist")
+Zalika.showall()
+Jill.showall()
+Jill.create_relationship(Zalika,"friend")
+
+Zalika.showall()
+Jill.showall()
+
+#my_group =
